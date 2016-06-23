17b3fb5a195a66c9bb101fd473ad4e72c85322a9
Eleanor Lee <ellie007@gmail.com>
2016-06-23 13:08:56 -0500

    binary tree traversal and search

---
+++ binary_tree.rb
@@ -0,0 +1,70 @@
+require 'pry'
+class BTree
+  attr_accessor :value, :parent, :left_child, :right_child
+
+  def initialize(value=nil, parent=nil)
+    @value = value
+    @parent = parent
+  end
+
+  # TODO: Implement a smart search, ie, O(log n)
+  def find(val)
+    if self.value == val
+      return true
+    else
+      self.value > val ? prune(self.left_child, val) : prune(self.right_child, val)
+    end
+  end
+
+  def prune(branch, val)
+    if branch.nil?
+      return false
+    elsif branch.value == val
+      return true
+    elsif branch.value > val
+      prune(branch.left_child, val)
+    elsif branch.value < val
+      prune(branch.right_child, val)
+    end
+  end
+
+  def breadth_first_traversal(&block)
+    queue = []
+    queue.push(self)
+
+    while !queue.empty?
+      current_node = queue.shift
+      block.call(current_node.value)
+      if current_node.left_child || current_node.right_child
+        queue.push(current_node.left_child) if current_node.left_child
+        queue.push(current_node.right_child) if current_node.right_child
+      end
+    end
+  end
+
+  ##### Our Code #######
+
+  def add(value)
+    if @value.nil?
+      @value = value
+    elsif value < @value
+      if @left_child.nil?
+        @left_child = BTree.new(value, self)
+      else
+        @left_child.add(value)
+      end
+    elsif value > @value
+      if @right_child.nil?
+        @right_child = BTree.new(value, self)
+      else
+        @right_child.add(value)
+      end
+    end
+  end
+
+  def depth_first_traversal(&block)
+    left_child.depth_first_traversal(&block) if left_child
+    block.call(value) if value
+    right_child.depth_first_traversal(&block) if right_child
+  end
+end
---
+++ binary_tree_spec.rb
@@ -0,0 +1,54 @@
+require 'pry'
+require_relative 'binary_tree'
+
+describe BTree do
+  describe "#depth_first_traversal" do
+    it "should traverse in order of smallest to largest" do
+      tree = create_tree_with_ordered_values([6, 9, 2, 4, 8])
+      string = ""
+
+      tree.depth_first_traversal { |x| string << x.to_s }
+
+      expect(string).to eq("24689")
+    end
+  end
+
+  describe "#breadth_first_traversal" do
+    it "should traverse each layer from left to right" do
+      tree = create_tree_with_ordered_values([6, 9, 2, 4, 8])
+      string = ""
+
+      tree.breadth_first_traversal { |x| string << x.to_s }
+
+      expect(string).to eq("62948")
+    end
+  end
+
+  describe "#find" do
+    it "should return true if it exists in the tree" do
+      tree = create_tree_with_ordered_values([6, 9, 2, 4])
+
+      result = tree.find(2)
+
+      expect(result).to eq(true)
+    end
+
+    it "should return false if it does not exist in the tree" do
+      tree = create_tree_with_ordered_values([6, 9, 2, 4])
+
+      result = tree.find(5)
+
+      expect(result).to eq(false)
+    end
+  end
+
+  def create_tree_with_ordered_values(ordered_values)
+    tree = BTree.new
+
+    ordered_values.each do |val|
+      tree.add(val)
+    end
+
+    tree
+  end
+end
